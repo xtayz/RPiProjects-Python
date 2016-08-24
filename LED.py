15d87141e7700f946767df41845fafe4cbf6330c
zhha <zhanghao.dev@gmail.com>
2016-08-24 22:23:04 +0800

    add files

---
+++ LED.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+import RPi.GPIO as GPIO
+import time
+
+PIN_NO = 12
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN_NO, GPIO.OUT)
+
+pwm = GPIO.PWM(PIN_NO, 50)
+pwm.start(0)
+
+try:
+	while True:
+		for i in xrange(0, 101, 1):
+			pwm.ChangeDutyCycle(i)
+			time.sleep(.03)
+		for i in xrange(100, -1, -1):
+			pwm.ChangeDutyCycle(i)
+			time.sleep(.03)
+			
+except KeyboardInterrupt:
+	pass
+
+pwm.stop()
+GPIO.cleanup()
+		
